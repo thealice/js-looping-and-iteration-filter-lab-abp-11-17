97c10b80ae9a3aaeca7fc2df735743f69d2f6a39
Alice Freda <alicefreda@gmail.com>
2017-11-15 02:03:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function findMatching (collection, string) {
   for (const driver of collection) {
-    if (driver === string;) {
+    if (function (driver) { return driver.toLowerCase === string.toLowerCase; } ) {
       return driver;
     }
   }
 }
 // filter(drivers, function (driver) { return driver.firstName === string; });
+// function (user) { return user.favoriteColor === 'Blue' && user.favoriteAnimal === 'Penguin'; }
