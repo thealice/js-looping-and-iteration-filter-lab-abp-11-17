13e8de9fa87f9b482c859c5e5927983021b8f454
Alice Freda <alicefreda@gmail.com>
2017-11-15 01:09:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-// Code your solution in this file
-let drivers = [];
-function findMatching(name) {
-
+const drivers = [{firstName: 'Bobby'}, {firstName: 'Sammy'}, {firstName: 'Sally'}, {firstName: 'Annette'}, {firstName: 'Sarah'}, {firstName: 'Bobby'}];
+function filter (collection, cb) {
+  for (const driver of collection) {
+    if (cb(driver)) {
+      return (driver.firstName);
+    }
+  }
 }
+
+// function findMatching(name) {
+// 
+// }
