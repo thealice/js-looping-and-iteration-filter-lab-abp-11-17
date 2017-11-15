e00c9bb79c4b9f036f507b66104dbeed2b2b440d
Alice Freda <alicefreda@gmail.com>
2017-11-15 01:46:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const drivers = [{firstName: 'Bobby'}, {firstName: 'Sammy'}, {firstName: 'Sally'}, {firstName: 'Annette'}, {firstName: 'Sarah'}, {firstName: 'Bobby'}];
-// function findMatching (collection, cb) {
-//   for (const driver of collection) {
-//     if (cb(driver)) {
-//       return (driver.firstName);
-//     }
-//   }
-// }
-// findMathing (drivers, function (string) { return user.favoriteColor === 'Blue' && user.favoriteAnimal === 'Penguin'; });
-
 function findMatching (collection, string) {
   for (const driver of collection) {
-    if (driver.firstName === string) {
+    if (driver === string;) {
       return (driver.firstName);
     }
   }
